79b69beced1cf1949d16731967b891a05f7c1702
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-28 12:58:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 from app.schema import FraudRequest
 from app.predict import predict_fraud
@@ -9,11 +11,16 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+# store prediction history
+history = []
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "history": history}
+    )
 
 
 @app.post("/predict")
@@ -21,4 +28,19 @@ def predict(request: FraudRequest):
 
     result = predict_fraud(request)
 
+    history.append(result)
+
     return result
+
+
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+    )
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
