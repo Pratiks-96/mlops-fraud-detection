d3a8188dc916219b549a1f6bb92205d35d1405ff
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-28 12:49:49 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from app.schema import FraudRequest
 from app.predict import predict_fraud
 
-app = FastAPI(title="Fraud Detection API")
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def health():
-    return {"status": "healthy"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/predict")
