ac095c4f2fb4381a73fd4076b9e71774ad418d6c
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-28 12:58:05 +0530

    Update predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,8 +1,13 @@
 import numpy as np
+import time
+
 from app.model_loader import model
+from app.metrics import prediction_counter, fraud_counter, prediction_latency
 
 
 def predict_fraud(request):
+
+    start = time.time()
 
     data = np.array([[
         request.amount,
@@ -13,8 +18,15 @@ def predict_fraud(request):
     ]])
 
     prediction = model.predict(data)[0]
-
     probability = model.predict_proba(data)[0][1]
+
+    prediction_counter.inc()
+
+    if prediction == 1:
+        fraud_counter.inc()
+
+    latency = time.time() - start
+    prediction_latency.observe(latency)
 
     return {
         "fraud": bool(prediction),
