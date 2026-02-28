f26fe7dfac06b1688787c3a87b9afe276c641ed5
Pratiks-96 <pratiksavant96@gmail.com>
2026-02-28 13:20:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import time
 import random
 
